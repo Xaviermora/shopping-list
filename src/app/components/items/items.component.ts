d779e12184d5da7b3d8c7b6b5c5a28b4224d454d
Xaviermora <xaviermoravega12@gmail.com>
2023-06-26 20:31:20 -0300

    Se hace estructurado html de los items

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-items',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent {
-
+  items: Item[] = [
+    {
+      id: 1,
+      title: 'Manzana',
+      price: 5,
+      quantity: 6,
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Pera',
+      price: 5,
+      quantity: 6,
+      completed: true
+    }
+  ]
 }
