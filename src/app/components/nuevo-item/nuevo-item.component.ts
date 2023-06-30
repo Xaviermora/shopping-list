3c3f66713c0257bc66348453009f00992a67aa26
Xaviermora <xaviermoravega12@gmail.com>
2023-06-30 01:48:17 -0300

    Añado estructurado y metodo ngSubmit

--- src/app/components/nuevo-item/nuevo-item.component.ts
+++ src/app/components/nuevo-item/nuevo-item.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-nuevo-item',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./nuevo-item.component.css']
 })
 export class NuevoItemComponent {
+  id!: string
+  title!: string
+  price!: number
+  quantity!: number
 
+  onSubmit(){
+    const item = {
+      id: this.id,
+      title: this.title,
+      price: this.price,
+      quantity: this.quantity
+    }
+  }
 }
