cb36e526f0162c194b2589651b35ed5cf398164c
Xaviermora <xaviermoravega12@gmail.com>
2023-06-30 15:19:20 -0300

    Añado estilos

--- src/app/components/nuevo-item/nuevo-item.component.ts
+++ src/app/components/nuevo-item/nuevo-item.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Item } from 'src/app/models/item';
 import { ItemService } from 'src/app/shared/services/item.service';
 
 @Component({
