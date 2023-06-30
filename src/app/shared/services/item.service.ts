63d7a684baf6fc184b82c8214a5480706bcdc630
Xaviermora <xaviermoravega12@gmail.com>
2023-06-30 14:54:58 -0300

    Creo metodos en el servicio conectados con el backend

--- src/app/shared/services/item.service.ts
+++ src/app/shared/services/item.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Item } from 'src/app/models/item';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
-  items: Item[] = [
-    {
-      id: '1',
-      title: 'Manzana',
-      price: 5,
-      quantity: 6,
-      completed: false
-    },
-    {
-      id: '2',
-      title: 'Pera',
-      price: 5,
-      quantity: 6,
-      completed: true
+  url: string = 'http://localhost:3000/items'
+  httpOptions = {
+    headers: {
+      'Content-Type': 'application/json'
     }
-  ]
-
-  constructor() { }
-
-  getItems(): Item[] {
-    return this.items
   }
 
-  addItem(item: Item){
-    this.items.unshift(item)
+  constructor(private http: HttpClient) { }
+
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.url)
+  }
+
+  addItem(item: Item): Observable<Item>{
+    return this.http.post<Item>(this.url, item, this.httpOptions)
+  }
+
+  toggleCompleted(item: Item): Observable<Item>{
+    return this.http.put<Item>(this.url+item.id, item, this.httpOptions)
+  }
+
+  deleteItem(item: Item): Observable<Item>{
+    return this.http.delete<Item>(this.url+item.id)
   }
 }
